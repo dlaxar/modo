eb0bbba5069661e733ac5629a72fd0f5e83d25ca
Daniel Laxar <i.dlaxar@gmail.com>
2014-06-30 14:10:10 +0200

    Connection setup

--- bootstrap.php
+++ bootstrap.php
@@ -1,2 +1,25 @@
 <?php
 
+use Doctrine\ODM\MongoDB\Configuration;
+use Doctrine\MongoDB\Connection;
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
+
+$loader = require_once(__DIR__ . '/vendor/autoload.php');
+$loader->add('Models', __DIR__);
+
+$connection = new Connection();
+
+$config = new Configuration();
+
+$config->setDefaultDB('modo');
+$config->setHydratorDir(__DIR__ . '/Hydrators');
+$config->setHydratorNamespace('Hydrators');
+$config->setProxyDir(__DIR__ . '/Proxies');
+$config->setProxyNamespace('Proxies');
+
+$config->setMetadataDriverImpl(AnnotationDriver::create(__DIR__ . '/Models'));
+
+AnnotationDriver::registerAnnotationClasses();
+
+$dm = \Doctrine\ODM\MongoDB\DocumentManager::create($connection, $config);
+
